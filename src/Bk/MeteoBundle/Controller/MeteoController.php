f63c3e1649219165154002eb13c7cd49bd45b2a8
Florent Machen <florent.machen@gmail.com>
2014-05-01 23:49:11 +0200

    premier jeu de données pour débuter l'affichage

--- src/Bk/MeteoBundle/Controller/MeteoController.php
+++ src/Bk/MeteoBundle/Controller/MeteoController.php
@@ -4,10 +4,14 @@ namespace Bk\MeteoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class MeteoController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('BkMeteoBundle:Meteo:index.html.twig', array('name' => 'meteo'));
+class MeteoController extends Controller {
+
+    public function indexAction() {
+        $param = $this->container->getParameter('OpenWeatherMap');
+        $fake = '../src/Bk/MeteoBundle/cache/fake/france.json';
+        $json = json_decode(file_get_contents($fake));
+        $cities = $json->list;
+        return $this->render('BkMeteoBundle:Meteo:index.html.twig', array('cities' => $cities));
     }
+
 }
